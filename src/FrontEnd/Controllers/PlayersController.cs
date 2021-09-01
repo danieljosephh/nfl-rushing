bfb82edf5e5004f3033575f7601db62561c8adc5
Danny Horodniczy <dhorodniczy@montrium.com>
2021-08-31 22:29:26 -0400

    - add PlayersController

---
+++ src/FrontEnd/Controllers/PlayersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nfl.Rushing.FrontEnd.WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PlayersController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            // conve
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(
+                "https://raw.githubusercontent.com/tsicareers/nfl-rushing/master/rushing.json");
+
+            var stats = await response.Content.ReadAsStringAsync();
+
+            return this.Ok(stats);
+        }
+    }
+}
