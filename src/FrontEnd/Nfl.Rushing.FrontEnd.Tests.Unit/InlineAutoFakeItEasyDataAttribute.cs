0ca767fa72f3ccbed0b2fa513fd35715c67a0cd5
Danny Horodniczy <dhorodniczy@montrium.com>
2021-09-16 00:54:06 -0400

    - add AutoFakeItEasyAttributes

---
+++ src/FrontEnd/Nfl.Rushing.FrontEnd.Tests.Unit/InlineAutoFakeItEasyDataAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using AutoFixture.Xunit2;
+
+namespace Nfl.Rushing.FrontEnd.Tests.Unit
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class InlineAutoFakeItEasyDataAttribute : InlineAutoDataAttribute
+    {
+        private readonly object[] _values;
+
+        public InlineAutoFakeItEasyDataAttribute(params object[] values)
+            : base(new AutoFakeItEasyDataAttribute(), values)
+        {
+            this._values = values;
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            var data = base.GetData(testMethod).ToList();
+
+            for (var i = 0; i < this._values.Length; i++)
+            {
+                data[0][i] = this._values[i];
+            }
+
+            return data;
+        }
+    }
+}
---
+++ src/FrontEnd/Nfl.Rushing.FrontEnd.Tests.Unit/MemberAutoFakeItEasyDataAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using AutoFixture.Xunit2;
+
+using Xunit;
+using Xunit.Sdk;
+
+namespace Nfl.Rushing.FrontEnd.Tests.Unit
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class MemberAutoFakeItEasyDataAttribute : CompositeDataAttribute
+    {
+        public MemberAutoFakeItEasyDataAttribute(string member)
+            : base(new MemberDataAttribute(member), new InfiniteDataAttribute(new AutoFakeItEasyDataAttribute()))
+        {
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            var result = base.GetData(testMethod);
+            return result;
+        }
+
+        // This attribute provide all the data to match the member data test case. It will not loop for ever. 
+        private class InfiniteDataAttribute : DataAttribute
+        {
+            private readonly DataAttribute _decorated;
+
+            public InfiniteDataAttribute(DataAttribute decorated)
+            {
+                this._decorated = decorated;
+            }
+
+            public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+            {
+                while (true)
+                {
+                    foreach (var dataSet in this._decorated.GetData(testMethod))
+                    {
+                        yield return dataSet;
+                    }
+                }
+            }
+        }
+    }
+}
